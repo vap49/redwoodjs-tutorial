41d3ece55cc885750a6ccdf27462e56577b449b7
root <root@RazerBlade15>
2022-09-22 23:41:31 -0400

    Done

---
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -0,0 +1,36 @@
+import { useAuth } from '@redwoodjs/auth'
+import { Link, routes } from '@redwoodjs/router'
+
+type BlogLayoutProps = {
+  children?: React.ReactNode
+}
+
+const BlogLayout = ({ children }: BlogLayoutProps) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+
+  return (
+    <>
+      <header>
+        <h1>
+          <Link to={routes.home()}>Redwood Blog</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            <li>
+              <Link to={routes.contact()}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default BlogLayout
---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+        <p>
+          Some text
+        </p>
+    </>
+  )
+}
+
+export default AboutPage
